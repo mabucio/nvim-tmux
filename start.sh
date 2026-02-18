0a393950ac933d6aea2442d68d99b3094b8aca27
Maciej-Bucio <maciej.bucio@protonmail.com>
2026-02-18 21:03:38 +0100

    Added tpm installation, fixed .tmux.conf localization

--- start.sh
+++ start.sh
@@ -2,8 +2,11 @@
 
 mkdir -p ~/.config/nvim/
 cp nvim/init.lua ~/.config/nvim
-cp tmux/.tmux.conf ~/.config
-
+cp tmux/.tmux.conf ~/
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt install fd-find
 sudo apt-get install ripgrep
+
+echo "Run: 'tmux source ~/.tmux.conf' in tmux then ctrl-space I to install tmux plugins"
+
 
